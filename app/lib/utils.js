e4318966011f7ff0beea48647ecbc74cdea9521f
Zach Sim <simjza@gmail.com>
2016-01-26 11:12:11 +1300

    npm run build cssimport index.css > bundle.css delete unnecessary files

--- app/lib/utils.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  chartWidth: function(root) {
-    var width = root.getBoundingClientRect().width;
-    var paddingLeft = Number(getComputedStyle(root).paddingLeft.slice(0,-2));
-    var paddingRight = Number(getComputedStyle(root).paddingRight.slice(0,-2));
-    return width-paddingLeft-paddingRight;
-  }
-};
